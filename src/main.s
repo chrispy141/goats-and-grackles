46f428d70f2f401ea1f24d832b4c4fef6dcb3bef
= <=>
2025-07-30 19:54:44 -0400

    Jump close, butnot jumping

--- src/main.s
+++ src/main.s
@@ -9,9 +9,23 @@ next_line:
 .segment "ZEROPAGE"
 spriteX: .res 1
 spriteY: .res 1
+verticalSpeed_lo: .res 1  ; somewhere in zero page
+verticalSpeed_hi: .res 1  ; somewhere in zero page
+horzizontalSpeed: .res 1
+tmp: .res 1
+goat_y_hi: .res 1
+goat_y_lo: .res 1
+fire_was_down: .res 1
 
 .segment "CODE"
 .export _start
+goat_sprite_x = $d000
+goat_sprite_y = $d001
+joystick = $dc00
+SPRITE_HIGH_BITS = $d010
+GOAT_SPRITE_HIGH_BIT = $1
+GRAVITY_HI = $00
+GRAVITY_LO = $01  ; try $01, $02, or $04 for different effects
 
 _start:
     sei                   ; Disable interrupts during setup
@@ -44,11 +58,11 @@ copy_sprite_loop:
     ; Enable sprite 0
     lda #$01
     sta $d015
-
+initialize_sprite:
     ; Set sprite 0 X/Y position
     lda #200
     sta $d000
-    lda #220
+    lda #120
     sta $d001
 
     ; Set sprite color (yellow)
@@ -93,9 +107,177 @@ COLOR_GREEN   = $05        ; Green (C64 color code)
         cpy #40
         bne @veg_loop
 
-        jmp loop
-loop:
-    jmp loop
+        jmp main_loop
+
+main_loop:
+    ldx $d000
+    ldy $d001
+
+    lda joystick
+    and #%00010000   ; fire button
+    beq try_jump     ; if 0, button is pressed
+
+    lda joystick
+    and #%00000100   ; left
+    beq move_left
+    lda joystick
+    and #%00001000   ; right
+    beq move_right
+    jsr experiance_gravity
+update:
+    jsr fall
+    ldy goat_y_hi
+    stx goat_sprite_x
+    sty goat_sprite_y
+
+    jsr delay
+
+    jmp main_loop
+
+on_ground:
+    sta tmp
+    lda #0
+    sta verticalSpeed_hi
+    sta verticalSpeed_lo
+    lda tmp
+    jmp update
+
+try_jump:
+    ldy goat_y_hi
+    cpy #220
+    bcc not_on_ground   ; Not on or above ground, ignore jump
+
+    ; On or below ground, perform jump!
+    lda #$F8            ; -8 in two's complement (tune as needed)
+    sta verticalSpeed_hi
+    lda #0
+    sta verticalSpeed_lo
+
+    jmp update
+
+not_on_ground:
+    jsr experiance_gravity
+
+move_left:
+    ;if in high range
+    lda SPRITE_HIGH_BITS ; >= 50 check to see if high bit is set
+    and #GOAT_SPRITE_HIGH_BIT
+    beq move_left_low_range ; high range not set, jump to low range
+    jmp move_left_high_range
+move_left_low_range:
+    cpx #25             ; compare to minimum X boundary (adjust 10 as needed)
+    bcc update  ; if less than 10, skip decrement (already at left edge)
+    jmp dec_x
+
+move_left_high_range:
+    ; check if low range is zero
+    cpx #$0
+    beq transition_to_low_range_x
+    ; low range is not zero, decrement and continue
+    jmp dec_x
+
+transition_to_low_range_x:
+    lda SPRITE_HIGH_BITS
+    eor #GOAT_SPRITE_HIGH_BIT
+    sta SPRITE_HIGH_BITS
+    jmp dec_x
+dec_x:
+    dex
+    jmp update
+move_right:
+    cpx #65  ;check to see if greater than 50
+    bcc inc_x ; less than 50, safe to increment in all cases
+    lda SPRITE_HIGH_BITS ; >= 50 check to see if high bit is set
+    and #GOAT_SPRITE_HIGH_BIT
+    beq move_right_low_range  ;high bit was not set, continue low range
+    ; high bit is set, and x >= 50, don't do anything, go back to update
+    jmp update
+move_right_low_range:
+    cpx #255 ; see if current x value is 255
+    bcc inc_x  ; if x < 255, skip to increment`
+    lda SPRITE_HIGH_BITS
+    eor #GOAT_SPRITE_HIGH_BIT
+    sta SPRITE_HIGH_BITS
+    jmp inc_x
+    
+inc_x:
+    inx
+    jmp update
+
+experiance_gravity:
+    cpy #220
+    bcs on_ground
+    clc
+
+    lda verticalSpeed_lo
+    adc #GRAVITY_LO
+    sta verticalSpeed_lo
+
+    lda verticalSpeed_hi
+    adc #GRAVITY_HI
+    sta verticalSpeed_hi
+
+    cmp #4
+    bcc :+
+    lda #4      ; clamp to terminal velocity
+    sta verticalSpeed_hi
+:
+    rts
+; After updating position in fall:
+
+; If falling (verticalSpeed_hi >= 0) AND goat_y_hi >= 220, trigger on_ground
+
+fall:
+    clc
+    lda goat_y_lo
+    adc verticalSpeed_lo
+    sta goat_y_lo
+
+    lda goat_y_hi
+    adc verticalSpeed_hi
+    sta goat_y_hi
+
+    ; Check for landing
+    lda verticalSpeed_hi
+    bmi no_landing        ; If negative (moving up), skip landing check
+
+    lda goat_y_hi
+    cmp #220
+    bcc no_landing        ; Not at/below ground
+
+    ; Landed! Clamp to ground and stop falling
+    lda #220
+    sta goat_y_hi
+    lda #0
+    sta goat_y_lo
+    lda #0
+    sta verticalSpeed_hi
+    sta verticalSpeed_lo
+    jmp update
+
+no_landing:
+    rts
+
+delay:
+    ldx #$ff
+wait1:
+    ldy #$08
+wait2:
+    dey
+    bne wait2
+    dex
+    bne wait1
+    rts
+
+long_delay:
+    ldx #$ff
+ld_wait1:
+    ldy #$ff
+ld_wait2:
+    dey
+    bne ld_wait2
+    dex
+    bne ld_wait1
 
 .segment "SPRITEDATA"
 smiley:
