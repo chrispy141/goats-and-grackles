956942a0b423fda9382463d5e70d0f06059d10a9
= <=>
2025-07-29 20:45:47 -0400

    Working

--- src/main.s
+++ src/main.s
@@ -6,9 +6,6 @@
 next_line:
     .word 0
 
-SPRITE_ADDR = $2000       ; Matches SPRITES memory in .cfg
-SPRITE_PTR  = SPRITE_ADDR / 64  ; VIC-II expects /64 pointer
-
 .segment "ZEROPAGE"
 spriteX: .res 1
 spriteY: .res 1
@@ -16,26 +13,42 @@ spriteY: .res 1
 .segment "CODE"
 .export _start
 
-
-
 _start:
     sei                   ; Disable interrupts during setup
-    lda #$93       ; PETSCII code for "clear screen"
-    jsr $ffd2      ; Output via CHROUT
+
+    ; clear screen
+    ldx #$00
+clear_loop:
+    lda #$20
+    sta $0400,x
+    sta $0500,x
+    sta $0600,x
+    sta $06E8,x
+    inx
+    bne clear_loop
 
     ; Set background and border color (blue)
     lda #$06
     sta $d020
     sta $d021
 
+    ; Copy sprite data to $2000 for VIC-II
+    ldx #0
+copy_sprite_loop:
+    lda smiley,x
+    sta $2000,x
+    inx
+    cpx #63
+    bcc copy_sprite_loop
+
     ; Enable sprite 0
     lda #$01
     sta $d015
 
     ; Set sprite 0 X/Y position
-    lda #100
+    lda #200
     sta $d000
-    lda #50
+    lda #150
     sta $d001
 
     ; Set sprite color (yellow)
@@ -43,54 +56,47 @@ _start:
     sta $d027
 
     ; Set sprite pointer in $07F8 (sprite 0)
-    lda #SPRITE_PTR
+    lda #$80
     sta $07f8
 
     cli                   ; Re-enable interrupts
+; Set up screen and color memory base addresses
+SCREEN        = $0400      ; C64 default screen RAM
+COLOR_RAM     = $D800      ; C64 color RAM
 
-print_msg:
-    lda #$0e              ; Switch to lowercase mode
-    jsr $ffd2
-    lda #'s'
-    jsr $ffd2
-    lda #'p'
-    jsr $ffd2
-    lda #'r'
-    jsr $ffd2
-    lda #'i'
-    jsr $ffd2
-    lda #'t'
-    jsr $ffd2
-    lda #'e'
-    jsr $ffd2
-    lda #$0d
-    jsr $ffd2
+CHAR_DIRT     = $C5        ; PETSCII code for '-'
+CHAR_VEG      = $D8        ; PETSCII code for '*'
+COLOR_BROWN   = $09        ; Brown (C64 color code)
+COLOR_GREEN   = $05        ; Green (C64 color code)
 
+        ldx #0
+
+; Fill last row (row 24) with DIRT ('-')
+        ldy #0
+@dirt_loop:
+        lda #CHAR_DIRT
+        sta SCREEN + 24*40,y
+        lda #COLOR_BROWN
+        sta COLOR_RAM + 24*40,y
+        iny
+        cpy #40
+        bne @dirt_loop
+
+; Fill row above last (row 23) with VEGETATION ('*')
+        ldy #0
+@veg_loop:
+        lda #CHAR_VEG
+        sta SCREEN + 23*40,y
+        lda #COLOR_GREEN
+        sta COLOR_RAM + 23*40,y
+        iny
+        cpy #40
+        bne @veg_loop
+
+        jmp loop
 loop:
     jmp loop
 
 .segment "SPRITEDATA"
 smiley:
-; 24x21 pixels = 63 bytes per sprite
-    .byte %00011111,%11100000,%00000000
-    .byte %00111111,%11110000,%00000000
-    .byte %01111111,%11111000,%00000000
-    .byte %11111000,%00011111,%00000000
-    .byte %11100111,%11100111,%00000000
-    .byte %11001111,%11110011,%00000000
-    .byte %11011111,%11111011,%00000000
-    .byte %10111100,%00111101,%00000000
-    .byte %10111000,%00011101,%00000000
-    .byte %10111000,%00011101,%00000000
-    .byte %10111100,%00111101,%00000000
-    .byte %10111111,%11111101,%00000000
-    .byte %11011111,%11111011,%00000000
-    .byte %11001111,%11110011,%00000000
-    .byte %11100111,%11100111,%00000000
-    .byte %11111000,%00011111,%00000000
-    .byte %01111111,%11111000,%00000000
-    .byte %00111111,%11110000,%00000000
-    .byte %00011111,%11100000,%00000000
-    .byte %00000000,%00000000,%00000000
-    .byte %00000000,%00000000,%00000000
-    .byte %00000000,%00000000,%00000000
+.include "sprites/sprite1_frame1.inc"
